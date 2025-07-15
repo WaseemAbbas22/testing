c2957cbe5699921929467333940a6100ffa3296f
Waseem Abbas <waseemabbasce22@gmail.com>
2025-07-15 05:46:49 -0700

    Updated FreeInspection.jsx

--- src/components/Blogs/FreeInspection.jsx
+++ src/components/Blogs/FreeInspection.jsx
@@ -6,7 +6,7 @@ const FreeInspection = () => {
       className="bg-cover bg-center rounded-lg shadow text-white mt-4 mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-0"
       style={{ backgroundImage: `url(${heroImg})` }}
     >
-      <div className="p-3 sm:p-4 md:p-6 lg:p-8 space-y-3 sm:space-y-4 bg-black bg-opacity-70 rounded-lg">
+      <div className="p-3 sm:p-4 md:p-6 lg:p-8 space-y-3 sm:space-y-4 bg-black bg-opacity-70 mb-2 rounded-lg">
         <div className="bg-opacity-80 px-2 py-2 rounded-t">
           <h3 className="text-base sm:text-lg md:text-xl font-semibold">
             Get a Free Inspection
@@ -23,7 +23,7 @@ const FreeInspection = () => {
         <input
           type="text"
           placeholder="Name"
-          className="border text-xs sm:text-sm px-3 py-2 rounded w-[60%] text-black h-8 sm:h-9 md:h-10"
+          className="border text-xs sm:text-sm px-3 py-2 rounded w-[100%] text-black h-8 sm:h-9 md:h-10"
         />
 
         <input
@@ -38,7 +38,7 @@ const FreeInspection = () => {
           className="border px-3 text-xs sm:text-sm py-2 rounded w-full text-black h-8 sm:h-9 md:h-10"
         />
 
-        <button className="bg-[#32A849] text-white px-1 sm:px-2 md:px-4 py-1 sm:py-2 md:py-1 rounded w-full text-xs sm:text-sm md:text-xs">
+        <button className="bg-[#32A849] text-white px-1 sm:px-2 md:px-4 py-1 sm:py-2 md:py-2 rounded w-full text-xs sm:text-sm md:text-xs">
           Send
         </button>
       </div>
