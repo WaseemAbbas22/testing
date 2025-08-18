79f693528f51990b046f0769f7d7c2c5f3e75fe6
Waseem Abbas <waseemabbasce22@gmail.com>
2025-08-18 10:23:23 -0700

    Updated

--- src/components/Pests/Rodent/roofrat.jsx
+++ src/components/Pests/Rodent/roofrat.jsx
@@ -2,7 +2,7 @@ import PestInfoPage from "../PestInfoPage";
 import TopBar from "../../TopBar";
 import Navbar from "../../Navbar";
 import ServicesHero from "../../Services/ServicesHero";
-import RodentHeroImg from "@/assets/Pests/Rodent/Roofhero.webp";
+import RodentHeroImg from "@/assets/Pests/Rodent/roofhero.webp";
 import Footer from "@/components/Footer";
 
 //import React from "react";
